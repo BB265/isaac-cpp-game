1c6749e1e52f43fe9b211bd70a5155f0be097ba9
cookiesnail <2914261830@qq.com>
2025-06-30 15:10:10 +0800

    gameview大框架和一个小demo

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
+#include"view/gameview.h"
+#include<optional>
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	gamewindow gameview;
+	sf::RenderWindow& current_window = gameview.window;
+    while (current_window.isOpen())
+    {
+        while (const std::optional event = current_window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                current_window.close();
+            }
+        }
+
+        gameview.draw_and_display();
+    }
 	return 0;
 }
