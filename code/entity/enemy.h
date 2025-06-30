4e805f9ae344f09e36aedbd31affb67c22abac84
Preisthe <574726144@qq.com>
2025-07-01 01:02:53 +0800

    implement enemy

--- code/entity/enemy.h
+++ code/entity/enemy.h
@@ -3,8 +3,21 @@
 
 class Enemy : public Entity {
 public:
-    Enemy(int x, int y) : Entity(x, y) {}
+    Enemy(int x, int y, const Entity* target) : Entity(x, y), target(target) {}
     ~Enemy() {}
+
+    // move
+    void setSpeed(int speed) { speed_ = speed; }
+    int getSpeed() const { return speed_; }
+    void update() override {
+        int dx = target->getX() - getX();
+        int dy = target->getY() - getY();
+        int distance = sqrt(dx * dx + dy * dy);
+        if (distance > 0) {
+            setX(getX() + dx * speed_ / distance);
+            setY(getY() + dy * speed_ / distance);
+        }
+    }
 
     // attack
     int getDamage() const { return damage_; }
@@ -17,4 +30,6 @@ public:
 private:
     int health_ = 1;
     int damage_ = 1;
+    int speed_ = 3;
+    const Entity* target;
 };
