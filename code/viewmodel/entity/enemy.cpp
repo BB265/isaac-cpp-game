a78076de17520702dd970f28d956fa341c7a311f
Preisthe <574726144@qq.com>
2025-07-02 20:00:03 +0800

    修改 end_window.png

--- code/viewmodel/entity/enemy.cpp
+++ code/viewmodel/entity/enemy.cpp
@@ -4,7 +4,6 @@
 void Enemy::update() {
     float dx = target->getX() - getX();
     float dy = target->getY() - getY();
-    // std::cout << "dx: " << dx << " dy: " << dy << std::endl;
     float distance = sqrt(dx * dx + dy * dy);
     if (distance != 0) {
         setX(getX() + dx * speed_ / distance);
@@ -18,9 +17,7 @@ void Enemy::collideWith(Entity* entity) {
         if (player->isInvincible()) {
             return;
         }
-        std::cout << "Enemy collide with player" << std::endl;
         int health = player->getHealth();
-        std:: cout << "player health: " << health - 1 << std::endl;
         player->setHealth(health - damage_);
     }
 }
