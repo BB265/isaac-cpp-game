b6a55c25a4bb025ea453233e29b96175e3de2998
cookiesnail <2914261830@qq.com>
2025-07-02 18:53:24 +0800

    完成gameview_test

--- test/gameview_test.cpp
+++ test/gameview_test.cpp
@@ -15,12 +15,23 @@ TEST(AssetManagerTest, TextureLoadingTest) {
     EXPECT_NE(textures.find("background"), textures.end());
     EXPECT_NE(textures.find("isaac"), textures.end());
     EXPECT_NE(textures.find("heart"), textures.end());
+    EXPECT_NE(textures.find("half_heart"), textures.end());
+    EXPECT_NE(textures.find("empty_heart"), textures.end());
+    EXPECT_NE(textures.find("enemy"), textures.end());
+    EXPECT_NE(textures.find("tear"), textures.end());
+    EXPECT_NE(textures.find("background"), textures.end());
 }
 
 // ≤‚ ‘∑ΩœÚ≈–∂œ∫Ø ˝
 TEST(GameViewTest, JudgeDirectionTest) {
     gamewindow window;
-    EXPECT_EQ(window.judgeDirection(1, 1), Direction::UpRight);
+    EXPECT_EQ(window.judgeDirection(1, 1), Direction::None);
     EXPECT_EQ(window.judgeDirection(2, 1), Direction::Up);
-    EXPECT_EQ(window.judgeDirection(0, 0), Direction::None);
+    EXPECT_EQ(window.judgeDirection(0, 0), Direction::DownLeft);
+    EXPECT_EQ(window.judgeDirection(0, 1), Direction::Down);
+    EXPECT_EQ(window.judgeDirection(1, 0), Direction::Left);
+    EXPECT_EQ(window.judgeDirection(0, 2), Direction::DownRight);
+    EXPECT_EQ(window.judgeDirection(1, 2), Direction::Right);
+    EXPECT_EQ(window.judgeDirection(2, 0), Direction::UpLeft);
+    EXPECT_EQ(window.judgeDirection(2, 2), Direction::UpRight;
 }
